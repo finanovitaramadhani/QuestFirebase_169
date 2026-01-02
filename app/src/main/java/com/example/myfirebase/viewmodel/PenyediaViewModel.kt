9f262b3fbbca8f85eff73088629449a59a882612
finanovita <finanovita479@gmail.com>
2026-01-02 18:25:36 +0700

    implementasi ViewModelFactory dengan repository siswa

---
+++ app/src/main/java/com/example/myfirebase/viewmodel/PenyediaViewModel.kt
@@ -0,0 +1,21 @@
+package com.example.myfirebase.viewmodel
+
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.myfirebase.repositori.AplikasiDataSiswa
+import androidx.lifecycle.ViewModelProvider
+
+
+
+fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa = (
+        this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as
+                AplikasiDataSiswa
+        )
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer { HomeViewModel(aplikasiDataSiswa().container.repositorySiswa) }
+        initializer { EntryViewModel(aplikasiDataSiswa().container.repositorySiswa) }
+    }
+}
